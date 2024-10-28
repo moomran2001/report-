ee3778f887cbff88701e4aa5d4e63a2d65ae627e
moomran2001 <momran@ritsol.com>
2024-10-28 13:15:41 +0300

    Add files via upload

---
+++ account_custom_report/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Custom Report",
+    'summary': "This Report For Account Custom Report",
+    'description': """ This Report For Account Custom Report """,
+    'author': "Mohamed ...",
+    'website': "https://google.com",
+    'category': 'Uncategorized',
+    'version': '17.0',
+    'depends': ['base', 'account'],
+    'data': [
+        'reports/report_action.xml',
+    ]
+}
+
